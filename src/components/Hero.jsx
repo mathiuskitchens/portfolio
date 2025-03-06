5f913a9aa8294f0c69a58e4642d6651eb22bbe99
Matthew Kitchens <mathiuskitchens@gmail.com>
2025-03-06 14:20:32 -0500

    fixed mispelling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,21 +11,21 @@ const Hero = () => {
 
   return (
     <div
-      className="hero bg-base-200 relative"
+      className="relative hero bg-base-200"
       style={{ height: "calc(100vh - 4rem)" }}
     >
       <div className="absolute top-20 md:top-0 md:left-0 md:relative min-h-124">
         <img
           src={Kitchens}
-          className="mask mask-squircle max-w-sm rounded-lg shadow-2xl grayscale transition duration-500 ease-in-out hover:grayscale-0 w-40 md:w-96 my-8 center mx-auto -mt-6 "
+          className="w-40 max-w-sm mx-auto my-8 -mt-6 transition duration-500 ease-in-out rounded-lg shadow-2xl mask mask-squircle grayscale hover:grayscale-0 md:w-96 center "
           alt="Image of Matthew Kitchens"
         />
 
-        <div className="my-auto mx-auto text-center">
-          <h1 className="text-5xl font-bold px-6">I'm Matt</h1>
-          <div className="divider px-6 w-3/4 md:w-1/2 mx-auto"></div>
+        <div className="mx-auto my-auto text-center">
+          <h1 className="px-6 text-5xl font-bold">I'm Matt</h1>
+          <div className="w-3/4 px-6 mx-auto divider md:w-1/2"></div>
 
-          <div className="text-xl mx-8 my-4">
+          <div className="mx-8 my-4 text-xl">
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
@@ -41,14 +41,14 @@ const Hero = () => {
                   .typeString("helping others")
                   .pauseFor(1000)
                   .deleteChars(15)
-                  .pasteString(" donuts, bulding and helping others")
+                  .pasteString(" donuts, building and helping others")
                   .start();
               }}
             />
           </div>
         </div>
         <div className="flex justify-center">
-          <Link to="/Contact" className="btn btn-primary text-2xl mt-12">
+          <Link to="/Contact" className="mt-12 text-2xl btn btn-primary">
             Hire Me
           </Link>
         </div>
